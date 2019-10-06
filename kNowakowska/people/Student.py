46dd59a1806503abc288600394f21a0f0868d08b
Aga D <dzbanuszek.agnieszka@gmail.com>
2019-10-06 12:31:03 +0200

    Changes as files, not submodule

---
+++ kNowakowska/people/Student.py
@@ -0,0 +1,17 @@
+from kNowakowska.people.Person import Person
+
+
+class Student(Person):
+
+    def setCV(self, exp, hobby):
+        self.cv = "Curriculum Vitae"
+        self.cv += "Name {}\nSurname{}\nAge{}\n".format(self.name, self.surname, self.getAge())
+        self.cv += "Doswiadczenie:"
+        for el in exp:
+            self.cv += el + "\n"
+        self.cv += "Zainteresowania:\n"
+        for el in hobby:
+            self.cv += el + "\n"
+
+    def presentCV(self):
+        print(self.cv)
