2210c87abe2bfca3999c02bd6c6204ac23fd0a4a
rokitka007 <k.popielarczyk91@gmail.com>
2019-10-12 12:05:24 +0200

    homework part

--- kNowakowska/people/Person.py
+++ kNowakowska/people/Person.py
@@ -1,11 +1,23 @@
 from datetime import datetime
+from random import random, randrange, uniform
+
+from kNowakowska.people.Skill import Skill
+
+
+class PersonException(Exception):
+    pass
+
 
 class Person():
 
-    def __init__(self, name, surname, dateOfBirth):
+    def __init__(self, name, surname, dateOfBirth, skills=None):
+        if skills is None:
+            skills = {}
+        self.skills = skills
         self.name = name
         self.surname = surname
-        self.dateOfBirth = datetime.strptime(dateOfBirth, "%d-%m-%y")
+        self.dateOfBirth = datetime.strptime(dateOfBirth, "%d-%m-%Y")
+
 
     def presentYourself(self):
         print("Hi, I'm {} {} and I'm {} years old".format(self.name, self.surname, self.getAge()))
@@ -20,3 +32,19 @@ class Person():
 
     def getSurname(self):
         return self.surname
+
+    def defineSkills(self, skills=None, automatic=True):
+        if not skills and automatic is True:
+            self.skills = {Skill.CHEMISTRY: random(), Skill.BIOLOGY: uniform(0,1), Skill.ENGLISH: uniform(0,1), Skill.HISTORY: 0, Skill.MATH: 0}
+        elif not skills and automatic is False:
+            raise PersonException("Give skills or try automatic skills creation")
+        else:
+            self.skills = skills
+
+    def presentSkills(self):
+        print("My skills:")
+        for k, v in self.skills.items():
+            print("{}: {}".format(k.name, v))
+
+    def _getSkillValue(self, skill):
+        return self.skills.get(skill)
